fbcc455dc5fadd8af4d8ac682d861b453227118d
Pro509 <pritishdugar509@gmail.com>
2024-05-30 01:58:43 +0400

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
+import Footer from '../components/Footer';
 import {
   useQuery,
   useMutation,
@@ -28,7 +30,9 @@ export default function RootLayout({
     <html lang="en">
       <QueryClientProvider client={queryClient}>
         <body className={inter.className}>{children}</body>
+        <Footer />
       </QueryClientProvider>
+      <Analytics />
     </html>
   );
 }
